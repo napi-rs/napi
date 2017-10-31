1a96800f38d315126a00709bc43de5144dd3fa92
Alexey Orlenko <eaglexrlnk@gmail.com>
2017-10-31 08:26:09 +0200

    feat(napi): implement NapiValue type checkers

--- crates/napi/src/value/mod.rs
+++ crates/napi/src/value/mod.rs
@@ -45,6 +45,46 @@ pub trait NapiValue {
     fn as_any(&self) -> NapiAny {
         NapiAny::with_value(self.env(), self.as_sys_value())
     }
+
+    fn instanceof(&self, constructor: &NapiObject) -> NapiResult<bool> {
+        let env = self.env();
+        let mut result = false;
+
+        env.handle_status(unsafe {
+            sys::napi_instanceof(
+                env.as_sys_env(),
+                self.as_sys_value(),
+                constructor.as_sys_value(),
+                &mut result,
+            )
+        })?;
+
+        Ok(result)
+    }
+
+    fn is_array(&self) -> NapiResult<bool> {
+        check_type(self, sys::napi_is_array)
+    }
+
+    fn is_arraybuffer(&self) -> NapiResult<bool> {
+        check_type(self, sys::napi_is_arraybuffer)
+    }
+
+    fn is_buffer(&self) -> NapiResult<bool> {
+        check_type(self, sys::napi_is_buffer)
+    }
+
+    fn is_error(&self) -> NapiResult<bool> {
+        check_type(self, sys::napi_is_error)
+    }
+
+    fn is_typedarray(&self) -> NapiResult<bool> {
+        check_type(self, sys::napi_is_typedarray)
+    }
+
+    fn is_dataview(&self) -> NapiResult<bool> {
+        check_type(self, sys::napi_is_dataview)
+    }
 }
 
 trait NapiValueInternal<'a>: NapiValue + 'a {
@@ -71,4 +111,22 @@ where
     })?;
 
     Ok(U::construct(env, coerced_value))
+}
+
+fn check_type<'a, T>(
+    value: &'a T,
+    napi_fn: unsafe extern "C" fn(sys::napi_env, sys::napi_value, *mut bool)
+        -> sys::napi_status,
+) -> NapiResult<bool>
+where
+    T: NapiValue + ?Sized,
+{
+    let env = value.env();
+    let mut result = false;
+
+    env.handle_status(unsafe {
+        napi_fn(env.as_sys_env(), value.as_sys_value(), &mut result)
+    })?;
+
+    Ok(result)
 }
