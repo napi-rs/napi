e030dcb2412bf0edd68614b9e53503c8bffbd6b9
Dmytro Nechai <nechaido@gmail.com>
2017-11-03 16:49:43 +0200

    feat(napi): implement NapiArrayBuffer

--- napi/src/lib.rs
+++ napi/src/lib.rs
@@ -6,9 +6,9 @@ mod value;
 
 pub use env::NapiEnv;
 pub use result::{NapiError, NapiErrorKind, NapiResult};
-pub use value::{AsNapiObject, NapiAny, NapiArray, NapiBoolean, NapiBuffer,
-                NapiNull, NapiObject, NapiString, NapiUndefined, NapiValue,
-                NapiValueType};
+pub use value::{AsNapiObject, NapiAny, NapiArray, NapiArrayBuffer,
+                NapiBoolean, NapiBuffer, NapiNull, NapiObject, NapiString,
+                NapiUndefined, NapiValue, NapiValueType};
 
 pub mod sys {
     pub use napi_sys::*;
